8c06e792992df98e83008b5c0d47f736dd17a8d9
barbute <jackhdemeterio@gmail.com>
2025-05-09 00:01:27 -0500

    Wrote new auto for assignment
    TODO Still need to fine tune it and the colors

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,15 @@
 #include "lib/telemetry.h"
 #include <iostream>
 #include <sstream>
+#include <array>
 
 using namespace vex;
 using signature = vision::signature;
 using code = vision::code;
 
 vex::brain Brain;
+
+std::string systemName = "S";
 
 std::string driveName = "D";
 vex::motor leftMotor(vex::PORT3, vex::gearSetting::ratio18_1, true);
@@ -39,6 +42,25 @@ vex::motor intakeMotor(vex::PORT1, vex::gearSetting::ratio18_1, false);
 vex::digital_in surfaceLimitSwitch(Brain.ThreeWirePort.D);
 
 const double DRIVE_SPEED_PCT = 20.0;
+
+const std::string ROW = "1"; // 1 or 2
+const std::string COLOR = "BLUE"; // GREEN, BLUE, or PINK
+
+const vex::color DESIRED_COLOR = vex::color::blue;
+
+// TODO Get these constants
+const double BOARD_COLOR_ROW_1_DISTANCE_MM = 1375.0;
+const double BOARD_COLOR_ROW_2_DISTANCE_MM = 950.0;
+
+// TODO Get these constants
+const std::array<double, 3> COLOR_ROW_1_GREEN = {0.0, 0.0, 0.0};
+const std::array<double, 3> COLOR_ROW_1_BLUE = {0.0, 0.0, 0.0};
+const std::array<double, 3> COLOR_ROW_1_PINK = {0.0, 0.0, 0.0};
+
+// TODO Get these constants
+const std::array<double, 3> COLOR_ROW_2_GREEN = {0.0, 0.0, 0.0};
+const std::array<double, 3> COLOR_ROW_2_BLUE = {0.0, 0.0, 0.0};
+const std::array<double, 3> COLOR_ROW_2_PINK = {0.0, 0.0, 0.0};
 
 const double PICKUP_DISTANCE_MM = 77.0;
 const double PREP_PLACE_DISTANCE_MM = 200.0;
@@ -79,6 +101,8 @@ int main() {
   Brain.Screen.setCursor(1, 1);
   wait(50, msec);
   Brain.Screen.clearScreen();
+  // Turn on color sensor light
+  opticalSensor.setLight(vex::ledState::on);
 
   // Instantiate subsystems
   subsystems::Drive drive(driveName, leftMotor, rightMotor, inertialSensor);
@@ -92,44 +116,116 @@ int main() {
   // scheduling logic
   if (RUN_AUTONOMOUS) {
     if (RUN_MAIN_AUTO) {
-      // Drive until in front of color row one or two
+      // Grab constants based on where the robot is running
+      double initialDistanceMM = 0.0;
+      std::array<double, 3> colorToSeek;
+
+      if (ROW == "1") {
+        initialDistanceMM = BOARD_COLOR_ROW_1_DISTANCE_MM;
+        if (COLOR == "GREEN") {
+          colorToSeek = COLOR_ROW_1_GREEN;
+        } else if (COLOR == "BLUE") {
+          colorToSeek = COLOR_ROW_1_BLUE;
+        } else if (COLOR == "PINK") {
+          colorToSeek = COLOR_ROW_1_PINK;
+        } else {
+          lib::Telemetry::writeOutput(systemName, "ERROR - COLOR(1) SELECTION INVALID");
+        }
+      } else if (ROW == "2") {
+        initialDistanceMM = BOARD_COLOR_ROW_2_DISTANCE_MM;
+        if (COLOR == "GREEN") {
+          colorToSeek = COLOR_ROW_2_GREEN;
+        } else if (COLOR == "BLUE") {
+          colorToSeek = COLOR_ROW_2_BLUE;
+        } else if (COLOR == "PINK") {
+          colorToSeek = COLOR_ROW_2_PINK;
+        } else {
+          lib::Telemetry::writeOutput(systemName, "ERROR - COLOR(2) SELECTION INVALID");
+        }
+      } else {
+        lib::Telemetry::writeOutput(systemName, "ERROR - ROW SELECTION INVALID");
+      }
+
+      // Drive until on side of color row
+      while (distanceSensor.objectDistance(vex::mm) > initialDistanceMM) {
+        drive.drive(vex::forward, DRIVE_SPEED_PCT, vex::velocityUnits::pct);
+      }
+      drive.stop();
+      wait(1, vex::sec);
 
       // Turn left 90 deg
+      drive.turnToAngle(vex::left, 90.0, vex::degrees);
 
       // Drive until on correct color row
+      // while (
+      //   opticalSensor.getRgb().red != colorToSeek[0] and
+      //   opticalSensor.getRgb().green != colorToSeek[1] and
+      //   opticalSensor.getRgb().blue != colorToSeek[2]
+      // ) {
+      //   drive.drive(vex::forward, DRIVE_SPEED_PCT, vex::velocityUnits::pct);
+      // }
+      while (opticalSensor.color() != DESIRED_COLOR) {
+        drive.drive(vex::forward, DRIVE_SPEED_PCT, vex::velocityUnits::pct);
+      }
+      drive.stop();
+      wait(1, vex::sec);
 
       // Turn left 90 deg
+      drive.turnToAngle(vex::left, 90.0, vex::degrees);
 
-      // Pre open claw
+      // Claw pre open
+      intake.setPositionRotations(CLAW_OPEN_ROTATIONS);
 
-      // Go to pickup position
+      // Elevator to pickup position
+      elevator.setPositionMM(PICKUP_HEIGHT_MM);
 
       // Drive until in front of cup
+      while (distanceSensor.objectDistance(vex::mm) > PICKUP_DISTANCE_MM) {
+        drive.drive(vex::forward, DRIVE_SPEED_PCT, vex::velocityUnits::pct);
+      }
+      drive.stop();
+      wait(1, vex::sec);
 
-      // Close claw
+      // Claw close
+      intake.setPositionRotations(CLAW_CLOSED_ROTATIONS);
 
-      // Stow cup
+      // Elevator to stow cup
+      elevator.setPositionMM(STOW_ELEVATOR_MM);
 
       // Turn left 90 deg
+      drive.turnToAngle(vex::left, 90.0, vex::degrees);
 
       // Drive to prep placing position
+      while (distanceSensor.objectDistance(vex::mm) > PREP_PLACE_DISTANCE_MM) {
+        drive.drive(vex::forward, DRIVE_SPEED_PCT, vex::velocityUnits::pct);
+  
+        wait(5, vex::msec);
+      }
+      drive.stop();
 
-      // Raise elevator to clearence
+      // Elevator raised to clearence
+      elevator.setPositionMM(CLEAR_TOP_BOX_HEIGHT_MM);
 
-      // Drive to placeing position
+      // Drive to place position
+      drive.driveDistance(vex::forward, PREP_PLACE_DISTANCE_MM - PLACE_DISTANCE_MM, vex::mm);
 
-      // Lower elevator to place
+      // Elevator lower to place
+      elevator.setPositionMM(PLACE_CUP_HEIGHT_MM);
 
-      // Open claw
+      // Claw open
+      intake.setPositionRotations(CLAW_OPEN_ROTATIONS);
 
-      // Raise elevator to clearence
+      // Elevator raise to clearence
+      elevator.setPositionMM(CLEAR_TOP_BOX_HEIGHT_MM);
 
-      // Close claw
+      // Claw close
+      intake.setPositionRotations(CLAW_CLOSED_ROTATIONS);
 
       // Drive backwards
+      drive.driveDistance(vex::reverse, PREP_PLACE_DISTANCE_MM, vex::mm);
 
-      // Lower elevator to pickup
-
+      // Elevator lower to pickup
+      elevator.setPositionMM(PICKUP_HEIGHT_MM);
     } else {
       // Prep Open claw
       intake.setPositionRotations(CLAW_OPEN_ROTATIONS);
@@ -179,7 +275,7 @@ int main() {
       // Drive backward slightly
       drive.driveDistance(vex::reverse, PREP_PLACE_DISTANCE_MM, vex::mm);
   
-      // Lower elevator to pickup oosition
+      // Lower elevator to pickup position
       elevator.setPositionMM(PICKUP_HEIGHT_MM);
     }
   } else {
